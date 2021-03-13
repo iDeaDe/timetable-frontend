4fd79e36bfa3b3d5e44a04bba1ad29722c4fb886
Anton Shutov <shutov01@mail.ru>
2021-03-14 03:57:09 +0500

    Теперь оно работает

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,122 @@
 <template>
   <div id="app">
+    <Loader v-if="isLoaderVisible" v-bind:loader-text="loaderText" />
     <Header v-bind:groups="groups.courses"
             v-bind:selected-group="selectedGroup"
-            v-on:groupselect="groupChanged" />
+            v-on:groupselect="groupChanged"
+            v-on:weekchange="weekChanged" />
     <div v-if="!isNaN(selectedGroup)">
-      <Filters />
       <Timetable v-bind:list="timetable.days" />
     </div>
-    <div v-else>
+    <div class="start-screen" v-else>
       Группа не выбрана
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
+import Loader from './components/Loader'
 import Header from './components/Header'
-import Filters from './components/Filters'
+//import Filters from './components/Filters'
 import Timetable from './components/Timetable'
 
-const API_URL = 'http://localhost:9000'
+const API_URL = 'https://timetable.ashutov.rocks'
 
 export default {
   name: 'App',
   components: {
+    Loader,
     Header,
-    Filters,
+    //Filters,
     Timetable
   },
   data: () => ({
     groups: {},
     timetable: {},
     selectedGroup: 0,
-    selectedWeek: 0
+    selectedWeek: 0,
+    // Лоадер
+    loaderText: '',
+    isLoaderVisible: true,
+    // Проверка загрузки данных
+    dataLoad: {
+      groups: false,
+      timetable: false
+    }
   }),
   created() {
     let vm = this
 
     vm.selectedGroup = parseInt(localStorage.getItem('selectedgroup'), 10)
+    if (window.location.search !== '') {
+      let group = parseInt((new URLSearchParams(window.location.search)).get('group'), 10)
+      if (!isNaN(group))
+        vm.selectedGroup = group
+    }
+    else if (!isNaN(vm.selectedGroup))
+      window.history.replaceState(
+        { group: vm.selectedGroup },
+        'Расписание',
+        `/?group=${vm.selectedGroup}`
+      )
 
-    // Fetch groups from API
-    fetch(`${API_URL}/api/groups`, { method: 'GET' })
+    // Load groups from API
+    axios.get(`${API_URL}/api/groups`, { responseType: 'json' })
       .then(resp => {
-        if (resp.status === 200) {
-          return resp.json()
-        }
+        if (resp.status === 200)
+          vm.groups = resp.data
+          vm.dataLoad.groups = true
       })
-      .then(data => vm.groups = data)
-      .catch(err => console.log(err))
+      .catch(err => console.log(err.toJSON()))
 
     vm.loadTimetable()
   },
   methods: {
+    showLoader() {
+      let vm = this
+      vm.isLoaderVisible = true
+
+      let interval = setInterval(() => {
+        vm.loaderText = 'Загрузка '
+
+        if (vm.dataLoad.groups && vm.dataLoad.timetable) {
+          vm.isLoaderVisible = false
+          clearInterval(interval)
+        }
+
+        if (vm.dataLoad.groups && !vm.dataLoad.timetable)
+          vm.loaderText += 'расписания'
+        else if (!vm.dataLoad.groups && vm.dataLoad.timetable)
+          vm.loaderText += 'групп'
+        else
+          vm.loaderText += 'данных'
+      }, 100)
+    },
+
     // Событие изменения группы
     groupChanged(group) {
       localStorage.setItem('selectedgroup', group)
-      this.selectedGroup = group
+      this.selectedGroup = parseInt(group, 10)
+      this.selectedWeek = 0
+      this.loadTimetable()
+      window.history.pushState(
+        { group: group },
+        'Расписание',
+        `/?group=${group}`
+      )
+    },
+
+    weekChanged(week) {
+      this.selectedWeek += week
+      this.loadTimetable()
     },
 
     loadTimetable() {
       let vm = this
+      vm.dataLoad.timetable = false
+      vm.showLoader()
 
       if (!isNaN(vm.selectedGroup)) {
         let params = new URLSearchParams()
@@ -66,32 +124,57 @@ export default {
         if (!isNaN(vm.selectedWeek))
           params.set('week', vm.selectedWeek.toString())
 
-        fetch(`${API_URL}/api/timetable?${params.toString()}`, { method: 'GET' })
+        axios.get(`${API_URL}/api/timetable?${params.toString()}`, { responseType: 'json' })
           .then(resp => {
             if (resp.status === 200) {
-              return resp.json()
+              vm.timetable = resp.data
+              vm.dataLoad.timetable = true
+              vm.selectedWeek = vm.timetable.week
             }
           })
-          .then(data => vm.timetable = data)
-          .catch(err => console.log(err))
+          .catch(err => console.log(err.toJSON()))
       }
+      else
+        vm.dataLoad.timetable = true
     }
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap');
+
+$mainColor: #2c3e50;
+
 *,
 body,
 html {
   margin: 0;
   padding: 0;
+  font-family: 'Inter', sans-serif;
+}
+
+body {
+  background-color: whitesmoke;
 }
 
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+  color: $mainColor;
+}
+
+button {
+  background-color: $mainColor;
+}
+
+.start-screen {
+  width: 100vw;
+  height: calc(100vh - 50px);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 20px;
 }
 </style>
--- src/components/Timetable.vue
+++ src/components/Timetable.vue
@@ -1,26 +1,39 @@
 <template>
-  <main class="timetable-grid">
-    <div v-for="chunk of listChunked" v-bind:key="chunk">
+  <main>
+    <div class="timetable-container"
+         v-for="chunk of chunkedList"
+         v-bind:key="chunk[0].day">
       <div class="day-block"
            v-for="day of chunk"
            v-bind:key="day.day">
-        {{ day.day }}
-        <table>
-          <tr v-for="lesson of day.lessons"
-              v-bind:key="lesson.number">
-            <div class="lesson-info"
-                 v-if="lesson.info.length > 0">
-              <td>{{ lesson.number }}</td>
-              <td>
-                <div class="lesson-time">
+        <div class="day-title">{{ day.day }}</div>
+        <table class="day-table">
+          <tbody class="lesson-info"
+                 v-for="lesson of day.lessons"
+                 v-bind:key="lesson.number">
+            <tr class="day-row"
+                v-for="(info, index) in lesson.info"
+                v-bind:key="Math.random() + info.cabinet">
+
+              <td class="lesson-number">
+                <span v-if="index === 0 || lesson.info.length === 1">
+                  {{ lesson.number }}
+                </span>
+              </td>
+              <td class="lesson-time">
+                <span v-if="index === 0 || lesson.info.length === 1">
                   <div>{{ lesson.start }}</div>
                   <div>{{ lesson.end }}</div>
-                </div>
+                </span>
               </td>
-              <td>{{ lesson.info[0].name }}</td>
-              <td>{{ lesson.info[0].cabinet }}</td>
-            </div>
-          </tr>
+              <td class="lesson-name"
+                  v-bind:class="info.canceled ? 'lesson-canceled' : ''">
+                {{ info.name }}<span class="lesson-teacher" v-if="info.teacher !== ''">({{ info.teacher }})</span>
+              </td>
+              <td class="lesson-cabinet"
+                  v-bind:class="info.canceled ? 'lesson-canceled' : ''">{{ info.cabinet }}</td>
+            </tr>
+          </tbody>
         </table>
       </div>
     </div>
@@ -38,41 +51,81 @@ export default {
   data: () => ({
      listChunked: []
   }),
-  beforeMount() {
-    let vm = this
-    console.log(chunk(vm.list, 3))
-  },
-  mounted() {
-    console.log(this.listChunked)
+  computed: {
+    chunkedList() {
+      return chunk(this.list, 3)
+    }
   }
 }
 </script>
 
-<style scoped>
-main {
+<style lang="scss">
+
+$mainBorder: #2c3e50;
+$innerBorder: #6782b5;
+
+.timetable-container {
+  margin-top: 10px;
   width: 100vw;
-}
-
-.timetable-grid {
-  display: grid;
-  grid-template-columns: 300px auto 300px auto 300px;
-}
-
-tr {
-  margin: 0 10px;
-  padding: 5px 0;
-  border-bottom: 1px solid #2c3e50;
-}
-
-tr:last-child {
-  border: none;
+  display: flex;
+  justify-content: space-around;
 }
 
 .day-block {
-  display: inline-block;
-  border: 1px solid #2c3e50;
+  flex: 1;
+  border: 1px solid $mainBorder;
   border-radius: 4px;
+  width: 26%;
+  margin: 0 10px 15px;
+
+  .day-title {
+    display: block;
+    margin: 0;
+    padding: 5px 10px;
+    border-bottom: 1px solid $mainBorder;
+    border-radius: 4px 4px 0 0;
+  }
+}
+
+.day-table {
+  width: 100%;
+  border-collapse: collapse;
+  margin: 0;
+}
+
+td {
+  padding: 5px 0;
+  border: none;
+}
+
+td:first-child {
+  padding-left: 10px;
+}
+
+tr:last-child {
+  padding-right: 10px;
+  border-radius: 0 0 4px 4px;
+}
+
+.day-row {
   padding: 10px;
+
+  &:last-child {
+    border: none;
+  }
+}
+
+.lesson-info {
+  border-bottom: 1px solid $innerBorder;
+
+  &:last-child {
+    border: none;
+  }
+}
+
+.lesson-canceled {
+  background-color: #ff8282;
+  border: none;
 }
 
 .lesson-time {
@@ -81,8 +134,31 @@ tr:last-child {
   font-size: 12px;
 }
 
-.lesson-info {
-  display: flex;
-  align-items: flex-end;
+.lesson-name {
+  padding-left: 3px;
+}
+
+.lesson-teacher {
+  font-size: 12px;
+}
+
+.lesson-cabinet {
+  text-align: center;
+}
+
+@media screen and (max-width: 900px) {
+  .timetable-container {
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .lesson-teacher {
+    display: block;
+  }
+
+  .day-block {
+    width: 80%;
+    margin: 10px 0;
+  }
 }
 </style>
